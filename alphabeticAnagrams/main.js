89b6580bcd7857f77d7bd9030e24bbe94e701fd8
nig <nig@beepanalytics.com>
2020-01-13 21:50:33 +0100

    wip

--- alphabeticAnagrams/main.js
+++ alphabeticAnagrams/main.js
@@ -35,44 +35,10 @@ function factorialOfEqualMultiplied(arr) {
 }
 
 
-function listPosition2(word) {
-    var indexer = {}; // D:3 B:1 A:0 C:2
-    var counts = []; // 2 1 1 1
-
-    var lettersCount = 0;
-    word.split("").sort().forEach(function(x){
-        if ( indexer[x] == undefined ) {
-            indexer[x] = lettersCount;
-            counts[lettersCount] = 0;
-            lettersCount ++;
-        }
-    });
-
-    var term = 1;
-    var sum = term;
-    word.split("").reverse().forEach(function(x, i){
-        var step = i + 1, idx = indexer[x];
-        counts[idx] ++;
-        term /= counts[idx];
-        for (var j = 0; j < idx; ++j) 
-            if (counts[j] != 0) 
-                sum += term * counts[j];
-        term *= step;
-    });
-    return sum;
-}
-
-console.time("e");
 console.log(listPosition("QUESTION")) // 24572
 console.log(listPosition("BOOKKEEPER")) // 10743
 
-console.timeEnd("e");
 
-console.time("e1");
-console.log(listPosition2("QUESTION")) // 24572
-console.log(listPosition2("BOOKKEEPER")) // 10743
-
-console.timeEnd("e1");
 
 
 
