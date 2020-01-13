49aea0ad47e7274e6caddb1ea9c23b1b7e983331
nig <nig@beepanalytics.com>
2020-01-13 21:50:08 +0100

    wip

--- alphabeticAnagrams/main.js
+++ alphabeticAnagrams/main.js
@@ -1,62 +1,78 @@
 function listPosition(word) {
 
-    var counter = 0;
-    var indexSum = 0;
+    function recursive(arr, counter) {
 
-    function rec(arr) {
+        var sortedArr = arr.slice(counter).sort();
 
-        if (arr.length === 0 ) return;
+        if (!sortedArr.length) return 0;
 
-        // console.log(countDuplicates(arr))
-
-        var indexOfChar = arr.indexOf(word[counter++]);
-
-        indexSum += (factorial(arr.length - 1) * indexOfChar) / countDuplicates(arr);
-
-        rec(word.split("").slice(counter).sort());
-
+        var indexOfChar = sortedArr.indexOf(word[counter++]);
+        
+        return ((factorial(sortedArr.length - 1) * indexOfChar) / factorialOfEqualMultiplied(sortedArr)) + recursive(wordArr, counter);
     }
 
-    rec(word.split("").slice(counter).sort())
-    return indexSum + 1;
+    const wordArr = word.split("");
+
+    return recursive(wordArr, 0) + 1;
 
 }
 
+const factorial = n => n <= 1 ? 1 : n * factorial(n - 1);
 
-function factorial (n) {
-
-    if (n === 1) {
-        return 1;
-    }
-    if (n === 0) return false;
-    return n * factorial(n - 1);
-
-}
-
-function countDuplicates(arr) {
+function factorialOfEqualMultiplied(arr) {
 
     var obj = {};
 
     arr.forEach(n => {
-        if (!obj.hasOwnProperty(n)) {
-            obj[n] = 1;
-          } else {
-            obj[n]++;
-          }
+        obj[n] = !obj.hasOwnProperty(n) ? 1 : obj[n] + 1;
     })
-
 
     var returnSum = 1;
     for ( let o in obj) {
-        returnSum *= obj[o] > 1 ? factorial(obj[o]) : 1
+        returnSum *= factorial(obj[o]);
     }
- 
     return returnSum;
-
 }
 
-// console.log(countDuplicates(["a", "a", "a", "b"]))
-console.log(listPosition("BOOKKEEPER"))
+
+function listPosition2(word) {
+    var indexer = {}; // D:3 B:1 A:0 C:2
+    var counts = []; // 2 1 1 1
+
+    var lettersCount = 0;
+    word.split("").sort().forEach(function(x){
+        if ( indexer[x] == undefined ) {
+            indexer[x] = lettersCount;
+            counts[lettersCount] = 0;
+            lettersCount ++;
+        }
+    });
+
+    var term = 1;
+    var sum = term;
+    word.split("").reverse().forEach(function(x, i){
+        var step = i + 1, idx = indexer[x];
+        counts[idx] ++;
+        term /= counts[idx];
+        for (var j = 0; j < idx; ++j) 
+            if (counts[j] != 0) 
+                sum += term * counts[j];
+        term *= step;
+    });
+    return sum;
+}
+
+console.time("e");
+console.log(listPosition("QUESTION")) // 24572
+console.log(listPosition("BOOKKEEPER")) // 10743
+
+console.timeEnd("e");
+
+console.time("e1");
+console.log(listPosition2("QUESTION")) // 24572
+console.log(listPosition2("BOOKKEEPER")) // 10743
+
+console.timeEnd("e1");
 
 
 
