ae6285cce1d9497d4106ce5096b9f313d3c0fcc7
nig <nig@beepanalytics.com>
2020-01-15 12:03:29 +0100

    wip

--- textAlignJustify/index.js
+++ textAlignJustify/index.js
@@ -3,78 +3,93 @@
 var text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis dolor mauris, at elementum ligula tempor eget. In quis rhoncus nunc, at aliquet orci. Fusce at dolor sit amet felis suscipit tristique. Nam a imperdiet tellus. Nulla eu vestibulum urna. Vivamus tincidunt suscipit enim, nec ultrices nisi volutpat ac."
 
 
-var justify = function(str, len) {
+var justify = function(str, lineWidth) {
+    const arr = createWordArrays(str, lineWidth);
+    return createFinalString(arr, lineWidth);
+}
 
+const createWordArrays = (str, lineWidth) => {
     var arr = [];
     var subArr = [];
     var word = "";
     var wordsLength = 0;
-    for (let i = 0; i < str.length; i++) {
-        let c = str[i];
-     
-        if (c === " " || i === str.length -1) {
 
-            if (i === str.length - 1) {
-                word += c;
-            }
+    for (let i = 0; i < str.length; i++) {
+
+        let c = str[i];
+
+        if (c === " ") {
             subArr.push(word);
-            if (word.length > len) return false;
+            if (word.length > lineWidth) return false;
             wordsLength += word.length;
-            word = ""
+            word = "";
             wordLength = 0;
         } else  {
             word += c;
         }
 
-        if (wordsLength + subArr.length - 1 > len) {
+        if (wordsLength + subArr.length - 1 > lineWidth) {
             var w = subArr.pop();
             arr.push(subArr);
             subArr = [w];
             wordsLength = w.length;
-        }
-       
+        }       
     }
-    arr.push(subArr);
 
+    subArr.push(getLastWordInString(str))
+    arr.push(subArr);
+    return arr;
+}
+
+const getLastWordInString = (str) => {
+    var cIndex = 1;
+    var word = ""
+    while (str[cIndex +1 ] !== " ") {
+        word += str[str.length - cIndex];
+        cIndex++;
+    }
+    return word.split("").reverse().join("");
+}
+
+function createFinalString(arr, lineWidth) {
     var finalstr = "";
     arr.forEach((wordsArr, index, thisArr)  => {
         if (index === thisArr.length - 1) return;
-        var totalLength = wordsArr.reduce((a,b) => a + b.length, 0);
-        var arr = calcSpaces(len - totalLength, wordsArr.length - 1);
+        
+        var wordsLength = wordsArr.reduce((a,b) => a + b.length, 0);
+        var arr = calcSpaces(lineWidth - wordsLength, wordsArr.length - 1);
         var counter = 0;
-        var newstr = ""
+        var newstr = "";
         wordsArr.forEach(w => {
             newstr += w + " ".repeat(arr[counter++]);
         })
         finalstr += newstr + "\n";
     })
     finalstr += arr[arr.length -1].join(" ");
-    return finalstr
+    return finalstr;
 }
 
-function calcSpaces(spaces, words) {
+function calcSpaces(nSpaces, nWords) {
 
-    if (words === 0) return [0];
+    if (nWords === 0) return [0];
 
-    var upper = Math.ceil(spaces/words);
-    var lower = Math.floor(spaces/words);
+    var upper = Math.ceil(nSpaces/nWords);
+    var lower = Math.floor(nSpaces/nWords);
 
     var arr = []
 
-    while (spaces != 0) {
+    while (nSpaces != 0) {
 
-        if (spaces % lower === 0 && (spaces / lower) + arr.length === words) {
-            spaces = spaces - lower;
+        if (nSpaces % lower === 0 && (nSpaces / lower) + arr.length === nWords) {
+            nSpaces = nSpaces - lower;
             arr.push(lower)
         } else {
-            spaces = spaces - upper;
+            nSpaces = nSpaces - upper;
             arr.push(upper);
         }
     }
     return arr;
 }
-
-
 
 var result = justify(text, 30);
 console.log(result)
